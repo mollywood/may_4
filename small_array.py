fff6eb4bf7eac03c0762228040ffe7c4835abc83
Molly Wood <mollywood@Interzone.localdomain>
2018-05-04 14:00:49 -0500

    update3

--- small_array.py
+++ small_array.py
@@ -1,5 +1,11 @@
 num_list = [4, 8, 42, 12, 99, 86, 2, 82]
-least = 0
+
+greatest = 0
+for num in num_list:
+    if num > greatest:
+        greatest = num
+
+least = greatest
 for num in num_list:
     if num < least:
         least = num
